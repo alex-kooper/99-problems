a98ffea445c2e9bfb5f30a6972e04266866548ab
alex.kooper@gmail.com <alex.kooper@gmail.com>
2017-05-30 15:35:44 -0400

    Added doctest to the solution.

--- haskell/Problem18.hs
+++ haskell/Problem18.hs
@@ -1,4 +1,10 @@
+module Problem18 where
 
+-- | Problem 18
+--
+-- >>> slice ['a','b','c','d','e','f','g','h','i','k'] 3 7
+-- "cdefg"
+--
 slice :: [a] -> Int -> Int -> [a]
 slice xs from to =  fmap snd . filter isInRange $ zip [1..] xs
   where isInRange (n, _) = n >= from && n <= to
