23908b61b11bd02c425efab9584c462e3f7a4abe
alex.kooper@gmail.com <alex.kooper@gmail.com>
2017-05-31 22:43:34 -0400

    Added 62A solution.

--- haskell/Problem62.hs
+++ haskell/Problem62.hs
@@ -1,6 +1,7 @@
 
 module Problem62 where
 
+import Data.Monoid
 import Tree
 
 -- | Problem 62
@@ -13,3 +14,14 @@ internals t = internals' t []
     internals' Empty xs = xs
     internals' (Branch _ Empty Empty) xs = xs
     internals' (Branch x l r) xs = x : internals' l (internals' r xs)
+
+
+-- | Problem 62A
+--
+-- >>> atLevel tree4 2
+-- [2,2]
+atLevel :: Tree a -> Int -> [a]
+atLevel Empty _  = []
+atLevel (Branch x l r) level
+      | level <= 1 = [x]
+      | otherwise  = atLevel l (level - 1) <> atLevel r (level - 1)
