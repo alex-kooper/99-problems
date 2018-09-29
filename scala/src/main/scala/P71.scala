5ccf58faee00a354d815ac7e72266060c734130c
alex.kooper@gmail.com <alex.kooper@gmail.com>
2018-09-29 15:36:08 -0400

    Added the solution to P71 problem.

---
+++ scala/src/main/scala/P71.scala
@@ -0,0 +1,21 @@
+import multiwaytree._
+
+object P71 extends App {
+  import P70._
+
+  implicit class MTreeOps[T](val tree: MTree[T]) {
+    def internalPathLength: Int = findAllPaths.map(_.length - 1).sum
+
+    type Path = List[MTree[T]]
+
+    def findAllPaths: List[Path] = {
+      tree.children.flatMap(child => addParent(tree, child, child.findAllPaths))
+    }
+
+    private[this] def addParent(parent: MTree[T], child: MTree[T], childPaths: List[Path]): List[Path] =
+      List(parent, child) :: childPaths.map(parent :: _)
+  }
+
+  println("string2MTree(\"afg^^c^bd^e^^^\").internalPathLength = " +
+           string2MTree("afg^^c^bd^e^^^").internalPathLength)
+}
