605e4f4c70eec761b4a9140380e3a4dc905dc9f0
alex.kooper@gmail.com <alex.kooper@gmail.com>
2018-09-28 22:02:05 -0400

    Started a new project scala, solved P70C problem.

---
+++ scala/src/main/scala/P70C.scala
@@ -0,0 +1,19 @@
+import multiwaytree.MTree
+
+object P70C extends App {
+
+  implicit class MTreeOps[T](tree: MTree[T]) {
+    def nodeCount: Int = {
+      tree.children.map(_.nodeCount).sum + 1
+    }
+  }
+
+  println("Problem P70C")
+  println("____________")
+
+  print("MTree('a', List(MTree('f'))).nodeCount = ")
+  println(MTree('a', List(MTree('f'))).nodeCount)
+
+  print("MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))).nodeCount = ")
+  println(MTree('a', List(MTree('f', List(MTree('g'))), MTree('c'), MTree('b', List(MTree('d'), MTree('e'))))).nodeCount)
+}
